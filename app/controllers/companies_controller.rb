e8b8290943422fdd42d63c0c24a07ce0187b71e5
Ariana Sherman <ariana.sherman@kellogg.northwestern.edu>
2025-02-18 22:23:31 +0000

    new post and save

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -26,7 +26,7 @@ class CompaniesController < ApplicationController
   #   # save Company row
   @company.save 
   #   # redirect user
-  redirect_to
+  redirect_to "/companies/"
   end
 
   # def edit
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,16 @@ class PostsController < ApplicationController
     # render posts/new view with new Post form
   end
 
-  # def create
-  #   # start with a new Post
-  #   # assign user-entered form data to Post's columns
-  #   # save Post row
-  #   # redirect user
-  # end
-
+  def create
+    @post=Post.new
+    # start with a new Post
+    # assign user-entered form data to Post's columns
+    @post["author"]=params["author"]
+    @post["body"]=params["body"]
+    @post["image"]=params["image"]
+    # save Post row
+    @post.save
+    # redirect user
+    redirect_to "/posts"
+  end
 end
